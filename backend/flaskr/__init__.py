bbc1ec047ece1678fbf77c3f68a8e3c6e1ff3e02
winchy82dev <nlahkim82@gmail.com>
2022-09-03 12:41:34 +0100

    delete comments

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -23,8 +23,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     setup_db(app)
-
-       
+ 
     # Setting up CORS. Allow '*' for origins.
     CORS(app)
    
@@ -42,7 +41,6 @@ def create_app(test_config=None):
         # sort alphabetically using order_by(Category.type)
         # instead of order_by(Category.id)
         categories = Category.query.order_by(Category.type).all()
-        # formated_categories = [category.format() for category in categories]
         formated_categories = {category.id:category.type for category in categories}
         return jsonify({
             'success' : True,
@@ -62,32 +60,14 @@ def create_app(test_config=None):
         categories_type = {category.id:category.type for category in categories}
         
         return jsonify({
-            'success' : True,
-            # This endpoint should return a list of questions,
-            'questions' : current_questions,
-            # number of total questions
-            'total_questions' : Question.query.count(),
-            # current category
-            'current_category' : None,
-            # categories
-            'categories' : categories_type
+            'success' : True, 
+            'questions' : current_questions, # list of questions
+            'total_questions' : Question.query.count(), # number of total questions
+            'current_category' : None,  # current category
+            'categories' : categories_type # categories
             })
 
-    """
-    @TODO:
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    """
-
-    """
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
-    
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
-    """
+    # An endpoint to DELETE question using a question ID
     @app.route('/questions/<int:question_id>', methods=["DELETE"])
     def delete_question(question_id):
         try:
@@ -101,6 +81,8 @@ def create_app(test_config=None):
             selection = Question.query.order_by(Question.id).all()
             current_questions = paginate_questions(request, selection)
             count = len(current_questions)
+            # trying to solve this issue (deleting the 11th or 21th or n1th question)
+            # recall the same page and not the page before it
             # if count % QUESTIONS_PER_PAGE == 0:
                 # go to home page
                 # return print('go the previous page')
@@ -115,12 +97,6 @@ def create_app(test_config=None):
         except:
             abort(422)
 
-    """
-    @TODO:
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
-    """
     # An endpoint to POST a new question,
     @app.route('/questions', methods=["POST"])
     def create_question():
@@ -145,12 +121,6 @@ def create_app(test_config=None):
             'categories' : formated_categories
         })
 
-    """
-    @TODO:
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
-    """
     # a POST endpoint to get questions based on a search term
     @app.route('/questions/search', methods=["POST"])
     def search():
@@ -161,7 +131,10 @@ def create_app(test_config=None):
             results = Question.query.filter(
                 Question.question.ilike("%{}%".format(searchTerm))
                 ).all()
+            
             current_questions = paginate_questions(request, results)
+            # get an issue here, the pagination doesn't work for results questions
+            # it return paginate question not thoses related with the query
             return jsonify({
                 'success' : True,
                 'questions' : current_questions,
@@ -169,12 +142,6 @@ def create_app(test_config=None):
                 'current_categories' : None
             })
 
-    """
-    @TODO:
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
-    """
     # an endpoint to get questions based on category.
     @app.route('/categories/<int:category_id>/questions')
     def get_questions_in_category(category_id):
@@ -186,7 +153,9 @@ def create_app(test_config=None):
         questions = Question.query.filter(
             Question.category == str(category_id)
             ).all()
+
         questions_in_category = paginate_questions(request, questions)
+        
         return jsonify({
             'success' : True,
             'questions' : questions_in_category,
@@ -194,17 +163,7 @@ def create_app(test_config=None):
             'current_category' : category['type'],
         })
 
-    """
-    @TODO:
-    Create a POST endpoint to get questions to play the quiz.
-    This endpoint should take category and previous question parameters
-    and return a random questions within the given category,
-    if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
-    """
+    # a POST endpoint to get questions to play the quiz
     @app.route('/quizzes', methods=["POST"])
     def play_quizz():
         try:
@@ -238,7 +197,6 @@ def create_app(test_config=None):
             print('some questions')
             current_question = questions[random.randint(0, len(questions)-1)]
             print(current_question)
-            forceEnd = False if questions else True 
         else:
             print('no question')
             return jsonify({
@@ -248,12 +206,7 @@ def create_app(test_config=None):
 
         return jsonify({
             'success': True,
-            'question' : current_question,
-            'quizCategory' : category['type'],
-            'showAnswer' : False,
-            'previousQuestions' : previous_questions,
-            'guess': '',
-            'forceEnd': forceEnd
+            'question' : current_question
             })
 
     """
